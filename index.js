f21dd5207a0880ae83ebec69400388edec9e233f
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-07-18 11:08:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+let currentUser='Grace Hopper';
+let welcomeMessage= "Welcome to Flatbook, ";
+
+welcomeMessage=welcomeMessage+currentUser+"!";
+
+const excitedWelcomeMessage="WELCOME TO FLATBOOK, GRACE HOPPER!";
+const shortGreeting="Welcome, "+"Grace"+"!";
