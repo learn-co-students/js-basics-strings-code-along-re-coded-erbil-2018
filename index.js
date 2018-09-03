4d7211cd27eb1e38ea9dc2628768636fc57780d2
blar khalid <blarharki@gmail.com>
2018-09-03 09:09:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Blar khalid';
+const welcomeMessage='Welcome to Flatbook, '+currentUser+'!';
+const  excitedWelcomeMessage=welcomeMessage.toUpperCase();
+let shortGreeting ='Welcome, ';
+ const firstInitial = currentUser[0];
+ const restOfName = currentUser.slice(1);
+      shortGreeting='Welcome, ' + firstInitial + '!';
