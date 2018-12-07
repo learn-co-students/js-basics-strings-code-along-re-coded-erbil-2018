5ead25aa5eb2d89cbc26ab70edd5b74a3a6afb68
Miray Talal Bko <meeray.k93@gmail.com>
2018-12-07 20:10:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser='Miray Talal'
+const welcomeMessage='Welcome to Flatbook, Miray Talal!'
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
